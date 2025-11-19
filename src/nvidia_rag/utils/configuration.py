niyatisingal/rag-nsingal

feat: add Redis-based global rate limiting for summary generation
- Implement global summary parallelization limit (default: 20) coordinated via Redis
- Refactor summary logic into dedicated summarization.py module
- Add per-worker semaphore with Redis-based global slot management
- Fix PENDING status to update immediately before NV-Ingest processing
- Add connection pooling (max 50) for Redis in summary status handler
- Track background tasks to prevent garbage collection and enable proper cleanup

src/nvidia_rag/utils/configuration.py
@@ -665,6 +665,12 @@ class SummarizerConfig(ConfigWizard):
         default=1.0,
         help_txt="Top-p sampling for the summarizer model (nucleus sampling)",
     )
+    max_parallelization: int = configfield(
+        "max_parallelization",
+        env_name="SUMMARY_MAX_PARALLELIZATION",
+        default=20,
+        help_txt="Maximum concurrent summaries across entire system (coordinated via Redis)",
+    )
 
 
 @configclass

NVIDIA-AI-Blueprints/rag

src/nvidia_rag/utils/configuration.py
@@ -71,8 +71,9 @@ class _ConfigBase(BaseModel):
                     if isinstance(raw_value, str) and len(raw_value) >= 2:
                         # More robust quote stripping: strip whitespace first, then quotes
                         raw_value = raw_value.strip()
-                        if (raw_value.startswith('"') and raw_value.endswith('"')) or \
-                           (raw_value.startswith("'") and raw_value.endswith("'")):
+                        if (raw_value.startswith('"') and raw_value.endswith('"')) or (
+                            raw_value.startswith("'") and raw_value.endswith("'")
+                        ):
                             raw_value = raw_value[1:-1]
                     env_values[field_name] = raw_value
 
@@ -630,6 +631,11 @@ class SummarizerConfig(_ConfigBase):
         default=1.0,
         env="SUMMARY_LLM_TOP_P",
         description="Nucleus sampling threshold for summary generation",
+    )
+    max_parallelization: int = Field(
+        default=20,
+        env="SUMMARY_MAX_PARALLELIZATION",
+        description="Maximum concurrent summaries across entire system (coordinated via Redis)",
     )
 
 