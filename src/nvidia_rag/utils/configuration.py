niyatisingal/rag-nsingal

Align summarization with nv-ingest token-based splitting
- Switch from character-based to token-based text chunking
- Use same tokenizer (e5-large-unsupervised) as nv-ingest
- Update defaults: max_chunk_length 50K→9K tokens, overlap 200→400
- Skip splitting in shallow extraction (summarization handles it)
- Update deployment configs (compose, helm) with new defaults

src/nvidia_rag/utils/configuration.py
@@ -644,14 +644,14 @@ class SummarizerConfig(ConfigWizard):
     max_chunk_length: int = configfield(
         "max_chunk_length",
         env_name="SUMMARY_LLM_MAX_CHUNK_LENGTH",
-        default=50000,
-        help_txt="Maximum chunk size in characters for the summarizer model",
+        default=9000,
+        help_txt="Maximum chunk size in tokens for the summarizer model",
     )
     chunk_overlap: int = configfield(
         "chunk_overlap",
         env_name="SUMMARY_CHUNK_OVERLAP",
-        default=200,
-        help_txt="Overlap between chunks for iterative summarization (in characters)",
+        default=400,
+        help_txt="Overlap between chunks for iterative summarization (in tokens)",
     )
     temperature: float = configfield(
         "temperature",

NVIDIA-AI-Blueprints/rag

src/nvidia_rag/utils/configuration.py
@@ -635,14 +635,14 @@ class SummarizerConfig(_ConfigBase):
         description="URL endpoint for summarization service",
     )
     max_chunk_length: int = Field(
-        default=50000,
+        default=9000,
         env="SUMMARY_LLM_MAX_CHUNK_LENGTH",
-        description="Maximum character length for chunks to summarize",
+        description="Maximum chunk size in tokens for the summarizer model",
     )
     chunk_overlap: int = Field(
-        default=200,
+        default=400,
         env="SUMMARY_CHUNK_OVERLAP",
-        description="Character overlap between chunks during summarization",
+        description="Overlap between chunks for iterative summarization (in tokens)",
     )
     temperature: float = Field(
         default=0.0,