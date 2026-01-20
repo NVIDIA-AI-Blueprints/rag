5c53590a0e34739e5e61beff029b5eba13e27863
Pranjal Doshi <pranjald@nvidia.com>
2026-01-20 15:18:23 +0530

    Set logging level to debug to see error trace

--- src/nvidia_rag/rag_server/main.py
+++ src/nvidia_rag/rag_server/main.py
@@ -2589,7 +2589,7 @@ class NvidiaRAG:
             )
 
             # Only show full traceback at DEBUG level
-            if logger.getEffectiveLevel() <= logging.INFO:
+            if logger.getEffectiveLevel() <= logging.DEBUG:
                 print_exc()
 
             if "[403] Forbidden" in str(e) and "Invalid UAM response" in str(e):
